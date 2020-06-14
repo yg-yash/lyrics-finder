b9808b3ec0fc52c136cb6bc3136564e1c230079a
yg-yash <yg123yash@gmail.com>
2020-06-14 01:39:35 -0700

    Updates

---
+++ precache-manifest.daa2b83491b6ad3aebaa4f4948930d2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e4732ffe8703304459cfe7db1b93ad5",
+    "url": "/lyrics-finder/index.html"
+  },
+  {
+    "revision": "9663a5d93086c7f7bc57",
+    "url": "/lyrics-finder/static/js/2.29743e15.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/lyrics-finder/static/js/2.29743e15.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bc9cd267408c08a87ac5",
+    "url": "/lyrics-finder/static/js/main.72b76a28.chunk.js"
+  },
+  {
+    "revision": "eb04debc5034b6df2276",
+    "url": "/lyrics-finder/static/js/runtime-main.74e87090.js"
+  }
+]);
