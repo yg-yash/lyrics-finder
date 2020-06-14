d7315ec8293d961ea02dbd34c24c9b59b60dc284
yg-yash <yg123yash@gmail.com>
2020-06-14 01:44:51 -0700

    added link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => (
   <div className="row">
     <div className="col bg-dark text-center ">
-      <p className="text-white pt-2 lead">Lyrics Finder</p>
+      <Link className="text-white pt-2 lead">Lyrics Finder</Link>
     </div>
   </div>
 );
